bd51bd325ebd75a03d356d9baa9f66b55533fff2
aymeeeeen <bouzianeaymen101@gmail.com>
2025-08-30 01:40:59 +0100

    Connect React frontend to Node.js backend with test API

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [backendData, setBackendData] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((res) => res.text())
+      .then((data) => setBackendData(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Beauty Skin AI App</h1>
+      <p>Backend says: {backendData}</p>
     </div>
   );
 }
